bb42186d8d0b337e7e6580654d9a468a399d47f2
nika0410 <wrydzyk@gmail.com>
2026-06-03 13:02:32 +0200

    Dodano Hello Controler z imieniem

---
+++ app/src/Controller/HelloController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Hello controller.
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * HelloController class.
+ */
+class HelloController
+{
+    /**
+     * Index action.
+     *
+     * @return Response HTTP response
+     */
+    #[Route(
+        '/hello',
+        name: 'hello_index',
+        methods: ['GET']
+    )]
+    public function index(Request $request): Response
+    {
+        $name = $request->query->getAlnum('name', 'World');
+
+        return new Response('Hello '.$name.'!');
+    }
+}
+
